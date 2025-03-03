ba21b728a49615738cf60bef1a6aaa003d8e4055
Syed Hussain Ather <shussainather@gmail.com>
2025-03-03 12:38:19 -0500

    elarboated

--- scistream/streams/views.py
+++ scistream/streams/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Stream
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def start_stream(request):
+    stream = Stream.objects.create(user=request.user, is_live=True, title="New Stream")
+    return JsonResponse({"message": "Stream started", "stream_id": stream.id})
+
+@login_required
+def stop_stream(request, stream_id):
+    stream = get_object_or_404(Stream, id=stream_id, user=request.user)
+    stream.is_live = False
+    stream.save()
+    return JsonResponse({"message": "Stream stopped"})
+
